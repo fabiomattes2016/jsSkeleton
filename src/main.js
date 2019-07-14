320d4dc368fe55677deb31bb0b16eb96e8515235
Fabio Mattes <fabiomattes2007@gmail.com>
2019-07-14 16:49:27 -0300

    Estrutura de Projetos

--- src/main.js
+++ src/main.js
@@ -6,7 +6,8 @@ class App {
         <p class="text-success">JS Skeleton - Estrutura Básica para projetos Javascript</p>
         <img src="https://media.giphy.com/media/93j3soUou1gaHJpY4Q/giphy.gif" alt="Skeleton" height="400">
         <br />
-        <hr />
+        <br />
+        <br />
         <a class="btn btn-success" href="https://github.com/fabiomattes2016/jsSkeleton" target="_blank">GitHub</a>
       </div>
     `;
