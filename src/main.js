8eb6e21e3e70c9a29c32ea6803b06bae28323c8b
Fábio Mattes <fabiomattes2007@gmail.com>
2020-10-11 15:55:27 -0300

    Adição de sistema de rotas

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
-class App {
-  render() {
-    let html = `
+const crossroads = require('crossroads')
+const hasher = require('hasher')
+const $ = require('jquery')(window)
+const Handlebars = require('handlebars')
+
+let home = `
       <br />
       <div class="text-center mx-auto">
         <p class="text-success">JS Skeleton - Estrutura Básica para projetos Javascript</p>
@@ -10,13 +13,61 @@ class App {
         <br />
         <a class="btn btn-success" href="https://github.com/fabiomattes2016/jsSkeleton" target="_blank">GitHub</a>
       </div>
-    `;
+    `
 
-    let app = document.getElementById("app");
-    app.innerHTML = html;
-  }
+let nova = `
+  <h1>Nova Rota</h1>
+`
+
+let ping = `
+  <div id='ping'>Loading...</div>
+  <script type='text/x-handlebars-template' id='ping-template'>
+    {{#each people}}
+    <div>
+      <strong>Nome: </strong> {{nome}}<br>
+      <strong>Email: </strong> {{email}}<br>
+      <strong>Cidade: </strong> {{endereco.cidade}}<br>
+    </div>
+    <br>
+    {{/each}}
+  </script>
+`
+
+crossroads.addRoute('/', () => {
+  render(home)
+});
+
+crossroads.addRoute('nova', () => {
+  render(nova)
+});
+
+crossroads.addRoute('ping', () => {
+  jQuery.ajax({
+    url: `http://localhost/backend/echo.php?t=${new Date().getTime()}`,
+  }).done(function (pong) {
+    render(ping)
+    // console.log(JSON.parse(pong))
+
+    const templateString = jQuery("#ping-template").html()
+    const personTemplate = Handlebars.compile(templateString)
+    const html = personTemplate({ people: JSON.parse(pong) })
+
+    jQuery("#ping").html(html)
+
+    // console.log(html)
+  })
+});
+
+function render(component) {
+  let app = document.getElementById("app");
+  app.innerHTML = ''
+  app.innerHTML = component
 }
 
-let chargeHtml = new App();
+function parseHash(newHash, oldHash) {
+  crossroads.parse(newHash);
+}
 
-chargeHtml.render();
+hasher.initialized.add(parseHash);
+hasher.changed.add(parseHash);
+hasher.init();
